b45b09374befe16ff5fc1a0a9d0b2acbdd0e0e53
theusaf <theusafyt@gmail.com>
2021-08-10 08:53:53 -0700

    Add Escort

---
+++ src/roles/Escort.js
@@ -0,0 +1,43 @@
+const TownRole = require("../TownRole"),
+  Action = require("../Action"),
+  {ROLE_TAG, ACTION_TAG, TARGET_FILTER, PRIORITY, ACTION_EXECUTE} = require("../enum");
+
+class Escort extends TownRole {
+  constructor() {
+    super("Escort");
+    this.setType(["town", "support"]);
+    this.setDescription("You are a beautiful person skilled in distraction.");
+    this.tags.add(ROLE_TAG.ROLEBLOCK_IMMUNE)
+  }
+
+  getDayActions() {
+    if (this.player.isDead()) {return;}
+    return [new DistractAction(this.player)];
+  }
+}
+
+class DistractAction extends Action {
+  constructor(initiator) {
+    super(initiator);
+    this.setPriority(PRIORITY.CANCELLERS);
+  }
+
+  isValidTarget(target) {
+    return TARGET_FILTER.LIVING(target) && TARGET_FILTER.NOT_SELF(target, this.initiator);
+  }
+
+  execute() {
+    const {target} = this,
+      {actions} = target;
+    if (target.tags.has(ROLE_TAG.ROLEBLOCK_IMMUNE)) {return;}
+    for (const action of actions) {
+      if (action.tags.has(ACTION_TAG.ROLEBLOCK_IMMUNE)) {
+        continue;
+      }
+      // TODO: replace with a Message object
+      action.cancel("Escort roleblock", this);
+    }
+  }
+}
+
+module.exports = Escort;
