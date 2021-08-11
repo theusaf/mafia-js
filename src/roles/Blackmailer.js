2830402f75f7b326b7cb1035329b465eccfdd808
theusaf <theusafyt@gmail.com>
2021-08-11 15:29:02 -0700

    add Blackmailer

---
+++ src/roles/Blackmailer.js
@@ -0,0 +1,33 @@
+const MafiaRole = require("../MafiaRole"),
+  Action = require("../Action"),
+  {ROLE_TAG, ACTION_TAG, TARGET_FILTER, PRIORITY, ACTION_EXECUTE} = require("../enum");
+
+// TODO: messages
+class Blackmailer extends MafiaRole {
+  constructor() {
+    super("Blackmailer");
+    this.setType(["mafia", "support"]);
+    this.setDescription("You are an eavesdropper who uses information to keep people quiet.");
+  }
+
+  getDayActions() {
+    if (this.player.isDead()) {return;}
+  }
+}
+
+class BlackmailAction extends Action {
+  constructor(initiator) {
+    super(initiator);
+    this.setPriority(PRIORITY.STATE_CHANGERS);
+  }
+
+  isValidTarget(target) {
+    return TARGET_FILTER.LIVING(target) && TARGET_FILTER.NOT_TEAM(target, this.initiator);
+  }
+
+  execute() {
+    this.target.effectData.isBlackmailed = true;
+  }
+}
+
+module.exports = Blackmailer;
