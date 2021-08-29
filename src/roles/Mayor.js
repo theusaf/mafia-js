34375897182e893bd6f9928254a6e401249440aa
theusaf <theusafyt@gmail.com>
2021-08-29 14:47:21 -0700

    Add Mayor

---
+++ src/roles/Mayor.js
@@ -0,0 +1,36 @@
+const TownRole = require("../TownRole"),
+  Action = require("../Action"),
+  {TARGET_FILTER, DEFENSE, PRIORITY, ACTION_EXECUTE} = require("../enum");
+
+class Mayor extends TownRole {
+
+  constructor() {
+    super("Mayor");
+    this.setDescription("You are the leader of the town.");
+    this.setType(["town", "support"]);
+    this.additionalInformation = {
+      isRevealed: false
+    };
+  }
+
+  getDayActions() {
+    if (this.player.isDead() || this.additionalInformation.isRevealed) {return;}
+    return [new RevealAction(this.player)];
+  }
+
+}
+
+class RevealAction extends Action {
+  constructor(initiator) {
+    super(initiator);
+    this.executeAt = ACTION_EXECUTE.IMMEDIATELY;
+  }
+
+  position() {return [];}
+
+  execute() {
+    this.initiator.isRevealed = true;
+  }
+}
+
+module.exports = Mayor;
